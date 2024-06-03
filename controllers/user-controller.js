df760db3a433e48674158a77b41e00aa9bbf9b37
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-06-03 16:49:34 -0400

    added the user routes and controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,20 +3,130 @@ const { ObjectId } = require('mongoose').Types;
 
 const userController = {
   // get all users
-  async getUsers(req, res) {},
+  async getUsers(req, res) {
+    try{
+      const users = await User.find();
+    res.json(users)
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
   // get single user by id
-  async getSingleUser(req, res) {},
+  async getSingleUser(req, res) {
+    try {
+    const user = await User.findOne({ _id: req.params.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: 'No student matches that ID' })
+    }
+
+    res.json(user)
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+  },
+
   // create a new user
-  async createUser(req, res) {},
+  async createUser(req, res) {
+    try{
+      const user = await User.create(req.body)
+      res.json(user)
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
   // update a user
-  async updateUser(req, res) {},
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        { $set: req.body},
+        { new: true, runValidators: true}
+      );
+      
+      if (!user) {
+        return res.status(404).json({ message: 'No such student exists' });
+      }
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
   // delete user (BONUS: and delete associated thoughts)
-  async deleteUser(req, res) {},
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+  
+      if (!user) {
+        return res.status(404).json({ message: 'No such user exists' });
+      }
+
+      const userFriends = await User.findOneAndUpdate(
+        { friends: req.params.userId },
+        { $pull: { friends: req.params.userId } },
+        { new: true, runValidators: true}
+      )
+
+      if (!userFriends) {
+        return res.status(404).json({
+          message: 'User deleted, user had no friends',
+        })
+      }
+
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 
   // add friend to friend list
-  async addFriend(req, res) {},
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
+        { new: true, runValidators: true},
+      );
+  
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user matches that id' });
+      }
+  
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
   // remove friend from friend list
-  async removeFriend(req, res) {},
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true, runValidators: true},
+      );
+  
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user matches that id' });
+      }
+  
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
-const {} = require("../../controllers/user-controller");
+const {
+    getUsers,
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require("../../controllers/user-controller");
 
 // /api/users
+router.route('/').get(getUsers).post(createUser)
 
 // /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser)
 
 // /api/users/:userId/friends/:friendId
+router.route('/:userId/friends').post(addFriend)
+
+router.route('/:userId/friends/:friendId').delete(removeFriend)
 
 module.exports = router;
